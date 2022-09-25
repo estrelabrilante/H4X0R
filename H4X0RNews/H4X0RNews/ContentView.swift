41d44645ad7bb1e0ba5f74526e7ceecc893a1354
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-09-25 19:47:48 -0400

    Networking in SwiftUI
    Hacker News App -- HN Search API

--- H4X0RNews/H4X0RNews/ContentView.swift
+++ H4X0RNews/H4X0RNews/ContentView.swift
@@ -27,11 +27,12 @@ struct ContentView: View {
     }
 }
 //Protocol called Identifiable : it must have id property which is a String
-struct Post : Identifiable{
+/*
+ struct Post : Identifiable{
     let id: String
     let title: String
     
-}
+}*/
 let posts = [Post(id:"1",title: "Hello"), Post(id: "2", title: "Bonjour"),Post(id: "3", title: "Namskram")]
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
